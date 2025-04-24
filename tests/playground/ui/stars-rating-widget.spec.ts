293aa5f819b44020fdec873eb733e308b4dd6d4a
matias.pili_redb <matias.pili@redb.ee>
2025-04-24 16:20:38 -0300

    Refactor tests to unify expect

--- tests/playground/ui/stars-rating-widget.spec.ts
+++ tests/playground/ui/stars-rating-widget.spec.ts
@@ -12,43 +12,35 @@ import { test, expect } from './fixtures/playground-pages';
 
 const expectValues = [
     {
-        rate: 0,
         marginTopImage: "0px",
         text: "Rate your experience",
         number: "0 out of 5"
     },
     {
-        rate: 1,
         marginTopImage: "0px",
         text: "I just hate it",
         number: "1 out of 5"
     },
     {
-        rate: 2,
         marginTopImage: "-135px",
         text: "I don't like it",
         number: "2 out of 5"
     },
     {
-        rate: 3,
         marginTopImage: "-270px",
         text: "This is awesome",
         number: "3 out of 5"
     },
     {
-        rate: 4,
         marginTopImage: "-405px",
         text: "I just like it",
         number: "4 out of 5"
     },
     {
-        rate: 5,
         marginTopImage: "-540px",
         text: "I just love it",
         number: "5 out of 5"
     }
-
-
 ];
 
 test.describe('Stars Rating Widget', () => {
@@ -57,176 +49,67 @@ test.describe('Stars Rating Widget', () => {
         await starsRatingWidgetPage.goto();
     });
 
-    test.describe('Without stars', () => {
-        test("Without stars, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("Without stars, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[0].marginTopImage);
-        });
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[0].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[0].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[0].number);
 
-        test("Without stars, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[0].text);
-        });
-
-        test("Without stars, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[0].number);
-        });
     });
 
-    test.describe('With 1 star', () => {
-        test("With 1 star, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("With 1 star, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
 
-            await starsRatingWidgetPage.pressStar1();
+        await starsRatingWidgetPage.pressStar1();
 
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[1].marginTopImage);
-
-        });
-
-        test("With 1 star, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar1();
-
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[1].text);
-
-        });
-
-        test("With 1 star, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar1();
-
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[1].number);
-
-        });
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[1].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[1].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[1].number);
     });
 
-    test.describe('With 2 stars', () => {
-        test("With 2 stars, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("With 2 stars, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
 
+        await starsRatingWidgetPage.pressStar2();
 
-            await starsRatingWidgetPage.pressStar2();
-
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[2].marginTopImage);
-        });
-
-        test("With 2 stars, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-
-            await starsRatingWidgetPage.pressStar2();
-
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[2].text);
-        });
-
-        test("With 2 stars, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-
-            await starsRatingWidgetPage.pressStar2();
-
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[2].number);
-        });
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[2].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[2].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[2].number);
     });
 
-    test.describe('With 3 stars', () => {
-        test("With 3 stars, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("With 3 stars, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
 
-            await starsRatingWidgetPage.pressStar3();
+        await starsRatingWidgetPage.pressStar3();
 
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[3].marginTopImage);
-        });
-
-        test("With 3 stars, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar3();
-
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[3].text);
-        });
-
-        test("With 3 stars, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar3();
-
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[3].number);
-        });
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[3].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[3].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[3].number);
     });
 
-    test.describe('With 4 stars', () => {
-        test("With 4 stars, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("With 4 stars, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
 
-            await starsRatingWidgetPage.pressStar4();
+        await starsRatingWidgetPage.pressStar4();
 
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[4].marginTopImage);
-        });
-
-        test("With 4 stars, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar4();
-
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[4].text);
-        });
-
-        test("With 4 stars, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar4();
-
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[4].number);
-
-        });
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[4].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[4].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[4].number);
     });
 
-    test.describe('With 5 stars', () => {
-        test("With 5 stars, should have correct image", async ({ starsRatingWidgetPage }) => {
+    test("With 5 stars, should have correct image, test and number", async ({ starsRatingWidgetPage }) => {
 
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
+        await expect(starsRatingWidgetPage.mainContent).toBeVisible();
 
-            await starsRatingWidgetPage.pressStar5();
+        await starsRatingWidgetPage.pressStar5();
 
-            await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[5].marginTopImage);
-
-        });
-
-        test("With 5 stars, should have correct text", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar5();
-
-            expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[5].text);
-        });
-
-        test("With 5 stars, should have correct number", async ({ starsRatingWidgetPage }) => {
-
-            await expect(starsRatingWidgetPage.mainContent).toBeVisible();
-
-            await starsRatingWidgetPage.pressStar5();
-
-            expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[5].number);
-        });
+        await expect(starsRatingWidgetPage.slideImg).toHaveCSS("margin-top", expectValues[5].marginTopImage);
+        expect(await starsRatingWidgetPage.getCurrentText()).toEqual(expectValues[5].text);
+        expect(await starsRatingWidgetPage.getCurrentNumb()).toEqual(expectValues[5].number);
     });
 });
