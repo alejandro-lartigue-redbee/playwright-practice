292b0a48b8f53f6effaeaad4d07b9c0c0939b132
matias.pili_redb <matias.pili@redb.ee>
2025-04-25 15:08:12 -0300

    Add Upload File test

--- tests/playground/ui/fixtures/playground-pages.ts
+++ tests/playground/ui/fixtures/playground-pages.ts
@@ -2,11 +2,13 @@ import { DynamicTablePage } from '../pages/dynamic-table-page';
 import { MouseHoverPage } from '../pages/mouse-hover-page';
 import { test as baseTest } from '@playwright/test';
 import { VerifyAccountPage } from '../pages/verify-account-page';
+import { UploadFilePage } from '../pages/upload-file-page';
 
 type PlaygroundPages = {
     verifyAccountPage: VerifyAccountPage;
     dynamicTablePage: DynamicTablePage;
     mouseHoverPage: MouseHoverPage;
+    uploadFilePage: UploadFilePage;
 }
 
 export const test = baseTest.extend<PlaygroundPages>({
@@ -18,6 +20,9 @@ export const test = baseTest.extend<PlaygroundPages>({
     },
     mouseHoverPage: async ({ page }, use) => {
         await use(new MouseHoverPage(page));
+    },
+    uploadFilePage: async ({ page }, use) => {
+        await use(new UploadFilePage(page));
     },
 });
 
