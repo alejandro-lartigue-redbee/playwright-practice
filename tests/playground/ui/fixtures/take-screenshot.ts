c1f22b73944d457ef213752a428491b338e4479a
matias.pili_redb <matias.pili@redb.ee>
2025-05-07 11:51:29 -0300

    Add 'TakeElementCaptureAndAttach'

--- tests/playground/ui/fixtures/take-screenshot.ts
+++ tests/playground/ui/fixtures/take-screenshot.ts
@@ -1,4 +1,4 @@
-import { Page, TestInfo } from "@playwright/test";
+import { Locator, Page, TestInfo } from "@playwright/test";
 import fs from 'fs';
 
 /**
@@ -15,7 +15,7 @@ export class TakeCapture {
      * @param testInfo - The Playwright `TestInfo` object used to attach artifacts to the test report.
      * @param deleteAfterAttach - Optional. If true, deletes the screenshot file after attaching it to the report. Default is true.
      */
-    async TakeCaptureAndAttach(
+    async TakeFullCaptureAndAttach(
         screenshotPath: string,
         screenshotName: string,
         page: Page,
@@ -29,6 +29,42 @@ export class TakeCapture {
 
         // Take screenshot
         await page.screenshot({ path: screenshotPath, fullPage: true });
+        // Attach to the report
+        await testInfo.attach(screenshotName, {
+            path: screenshotPath,
+            contentType: 'image/png',
+        });
+
+        // Optionally, remove the screenshot after attaching it to the report.
+        // It helps to keep the screenshots folder clean.
+        if (deleteAfterAttach && fs.existsSync(screenshotPath)) {
+            fs.unlinkSync(screenshotPath);
+        }
+    }
+
+    /**
+     * Takes a element screenshot, attaches it to the test report, and deletes the local file afterwards.
+     *
+     * @param screenshotPath - The file system path where the screenshot will be temporarily saved.
+     * @param screenshotName - The name that will appear in the Playwright test report.
+     * @param element - The Playwright `Element` object from which the screenshot will be taken.
+     * @param testInfo - The Playwright `TestInfo` object used to attach artifacts to the test report.
+     * @param deleteAfterAttach - Optional. If true, deletes the screenshot file after attaching it to the report. Default is true.
+     */
+    async TakeElementCaptureAndAttach(
+        screenshotPath: string,
+        screenshotName: string,
+        element: Locator,
+        testInfo: TestInfo,
+        deleteAfterAttach: boolean = true
+    ) {
+        // Create the screenshots folder if it doesn't exist
+        if (!fs.existsSync('screenshots')) {
+            fs.mkdirSync('screenshots');
+        }
+
+        // Take screenshot
+        await element.screenshot({ path: screenshotPath });
         // Attach to the report
         await testInfo.attach(screenshotName, {
             path: screenshotPath,
